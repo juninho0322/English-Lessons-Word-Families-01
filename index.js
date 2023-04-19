5a46fda9b3bbfed2a7294ce8ce33abe67f58ac42
Juninho <juninho0322@gmail.com>
2023-04-19 21:25:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+// alert("Test");
+
+
+
+
+var a = document.getElementById("playAbility"); 
+
+function playAbilityAudio() { 
+  a.play(); 
+} 
+
+var b = document.getElementById("playDisability"); 
+
+function playDisabilityAudio() { 
+  b.play(); 
+} 
+
+
+var c = document.getElementById("playInability"); 
+
+function playInabilityAudio() { 
+  c.play(); 
+} 
+
+var d = document.getElementById("playAble"); 
+
+function playAbleAudio() { 
+  d.play(); 
+} 
+
+var e = document.getElementById("playUnable"); 
+
+function playUnableAudio() { 
+  e.play(); 
+} 
+
+var f = document.getElementById("playDisabled"); 
+
+function playDisabledAudio() { 
+  f.play(); 
+} 
+
+var g = document.getElementById("playEnable"); 
+
+function playEnableAudio() { 
+  g.play(); 
+} 
+
+var h = document.getElementById("playDisable"); 
+
+function playDisableAudio() { 
+  h.play(); 
+} 
+
+var i = document.getElementById("playAbly"); 
+
+function playAblyAudio() { 
+  i.play(); 
+} 
+
+var j = document.getElementById("playPhrase1"); 
+
+function playPhrase1Audio() { 
+  j.play(); 
+} 
+
+var l = document.getElementById("playPhrase2"); 
+
+function playPhrase2Audio() { 
+  l.play(); 
+} 
+
+var m = document.getElementById("playPhrase3"); 
+
+function playPhrase3Audio() { 
+  m.play(); 
+} 
+
+var n = document.getElementById("playPhrase4"); 
+
+function playPhrase4Audio() { 
+  n.play(); 
+} 
+
+var o = document.getElementById("playPhrase5"); 
+
+function playPhrase5Audio() { 
+  o.play(); 
+} 
+
+var p = document.getElementById("playPhrase6"); 
+
+function playPhrase6Audio() { 
+  p.play(); 
+} 
+
+var q = document.getElementById("playPhrase7"); 
+
+function playPhrase7Audio() { 
+  q.play(); 
+} 
+
+var r = document.getElementById("playPhrase8"); 
+
+function playPhrase8Audio() { 
+  r.play(); 
+} 
+
+var s = document.getElementById("playPhrase9"); 
+
+function playPhrase9Audio() { 
+  s.play(); 
+} 
